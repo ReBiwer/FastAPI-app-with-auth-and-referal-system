ed03fb3e21c2aa6088f1a33a0a561f3d959ead7b
vladimir <vovka1998@gmail.com>
2025-02-20 11:35:25 +0300

    deleted model Role

--- app/auth/dao.py
+++ app/auth/dao.py
@@ -1,10 +1,7 @@
 from app.dao.base import BaseDAO
-from app.auth.models import User, Role
+from app.auth.models import User
 
 
 class UsersDAO(BaseDAO):
     model = User
 
-
-class RoleDAO(BaseDAO):
-    model = Role
