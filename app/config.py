90ee65992f46eb9f94c028c4f13197d284c78857
vladimir <vovka1998@gmail.com>
2025-02-21 16:27:31 +0300

    added conf for SMTP

--- app/config.py
+++ app/config.py
@@ -7,6 +7,16 @@ class Settings(BaseSettings):
     DB_URL: str = f"sqlite+aiosqlite:///{BASE_DIR}/data/db.sqlite3"
     SECRET_KEY: str
     ALGORITHM: str
+    MAIL_USERNAME: str
+    MAIL_PASSWORD: str
+    MAIL_FROM: str
+    MAIL_PORT: int = 587
+    MAIL_SERVER: str
+    MAIL_FROM_NAME: str
+    MAIL_STARTTLS: bool = True
+    MAIL_SSL_TLS: bool = False
+    USE_CREDENTIALS: bool = True
+    VALIDATE_CERTS: bool = True
 
     model_config = SettingsConfigDict(env_file=f"{BASE_DIR}/.env")
 
