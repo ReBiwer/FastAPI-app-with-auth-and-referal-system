67f8c239154d902c7667d10ee5a3aa7f698bbced
vladimir <vovka1998@gmail.com>
2025-02-24 08:26:32 +0300

    added schema for Referrer

--- app/referral_system/schemas.py
+++ app/referral_system/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field, ConfigDict
+from pydantic import BaseModel, Field, ConfigDict, EmailStr
 
 
 class CreateReferralCode(BaseModel):
@@ -8,3 +8,15 @@ class CreateReferralCode(BaseModel):
 class ReferralCode(CreateReferralCode):
     code: str = Field(description="Реферальный код")
 
+
+class Referrer(BaseModel):
+    first_name: str
+    last_name: str
+    email: EmailStr
+    referrals: list["Referrer"]
+
+    class Config:
+        orm_mode = True
+        allow_population_by_field_name = True
+
+Referrer.model_rebuild()
